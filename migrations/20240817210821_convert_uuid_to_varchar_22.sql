c6614dfca5f90cedfec6fdf3c3c398420be09b9c
Mano Sriram <mano.sriram0@gmail.com>
2024-08-18 02:41:11 +0530

    add migration script

---
+++ migrations/20240817210821_convert_uuid_to_varchar_22.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE outagealertio.public.monitor DROP constraint monitor_pkey CASCADE;
+alter table outagealertio.public.monitor alter column id drop default;
+ALTER TABLE outagealertio.public.monitor ALTER COLUMN id TYPE varchar(22);
+ALTER TABLE outagealertio.public.project DROP constraint project_pkey CASCADE;
+alter table outagealertio.public.project alter column id drop default;
+ALTER TABLE outagealertio.public.project ALTER COLUMN id TYPE varchar(22);
+ALTER TABLE outagealertio.public.ping ALTER COLUMN id TYPE varchar(22);
+alter table outagealertio.public.ping alter column id drop default;
+ALTER TABLE outagealertio.public.project add PRIMARY KEY(id);
+ALTER TABLE outagealertio.public.monitor add PRIMARY KEY(id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
