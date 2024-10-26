0e0792c73dbbd8f77a17c6cee5e5832390203f60
Mano Sriram <mano.sriram0@gmail.com>
2024-10-26 05:38:11 +0530

    fix: do not use uuid-ossp extension

--- migrations/20240815191904_add_monitor_table.sql
+++ migrations/20240815191904_add_monitor_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+--  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS monitor (
 		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 		name varchar(64) NOT NULL,
--- migrations/20240817071857_add_ping_table.sql
+++ migrations/20240817071857_add_ping_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+--  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS ping (
 		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 		monitor_id UUID REFERENCES monitor(id) NOT NULL,
