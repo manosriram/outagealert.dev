d9cec3d57a93e05fc932c4a6ff2f92098e8a3491
Mano Sriram <mano.sriram0@gmail.com>
2024-11-01 23:19:09 +0530

    Ignore errors psql script

--- init-database.sh
+++ init-database.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
 
 echo "initializing database";
-psql -v ON_ERROR_STOP=1 -U postgres <<-EOSQL
+psql -U postgres <<-EOSQL
     CREATE DATABASE outagealert;
     GRANT ALL PRIVILEGES ON DATABASE outagealert TO docker;
 EOSQL
