f02496004b1ae92e311ffdcc09a2447f3fd0b053
Mano Sriram <mano.sriram0@gmail.com>
2024-11-11 21:17:18 +0530

    update deploy.sh to create networks if not exists

--- deploy.sh
+++ deploy.sh
@@ -12,46 +12,54 @@ docker push manosriram/outagealert:app
 
 # Deploy command
 ssh -v root@$OUTAGEALERT_IP "
-  # Docker login
-	export DOPPLER_TOKEN=$DOPPLER_TOKEN
-  echo $DOCKER_REGISTRY_PAT | docker login -u manosriram --password-stdin
-  
-  # Remove old image if exists
-  yes | (docker rmi manosriram/outagealert:app 2>/dev/null || true)
-  
-  # Cleanup
-  docker system prune -a
-  docker volume prune -a
-  
-  # Update code
-  cd /root/dev/outagealert.io
-  git pull origin main
-  
-  # Pull new image
-  docker pull manosriram/outagealert:app
-  
-  # Stop existing stack and remove network
-  docker stack rm outagealert
+		# Docker login
+		export DOPPLER_TOKEN=$DOPPLER_TOKEN
+		echo $DOCKER_REGISTRY_PAT | docker login -u manosriram --password-stdin
 
-  # Wait for stack to be fully removed
-  while docker stack ls | grep -q 'outagealert'; do
-    echo 'Waiting for stack to be removed...'
-    sleep 5
-  done
+		NETWORK_NAME=outagenet
 
-	sleep 5
-  
-  # Deploy stack
-	docker stack config -c docker-compose.yml | docker stack deploy -c - outagealert
-  
-  # Setup Doppler
-  curl -Ls https://cli.doppler.com/install.sh | sh
-  doppler configure set token $DOPPLER_TOKEN
-  
-  # Start containers
-  # DOPPLER_TOKEN=$DOPPLER_TOKEN docker-compose -f /root/dev/outagealert.io/docker-compose.yml up --force-recreate -d
-  
-  # Final cleanup
-  # docker system prune -a
-  # docker volume prune -a
+		# Remove old image if exists
+		yes | (docker rmi manosriram/outagealert:app 2>/dev/null || true)
+
+		# Cleanup
+		docker system prune -a
+		docker volume prune -a
+
+		# Update code
+		cd /root/dev/outagealert.io
+		git pull origin main
+
+		# Pull new image
+		docker pull manosriram/outagealert:app
+
+		# Stop existing stack and remove network
+		docker stack rm outagealert
+
+		# Wait for stack to be fully removed
+		while docker stack ls | grep -q 'outagealert'; do
+				echo 'Waiting for stack to be removed...'
+				sleep 5
+		done
+
+		sleep 5
+
+		if docker network inspect $NETWORK_NAME &> /dev/null; then
+				echo "Network '$NETWORK_NAME' already exists."
+				NETWORK_EXISTS=true
+		else
+				NETWORK_EXISTS=false
+		fi
+
+		# Create the network if it doesn't exist
+		if [ "$NETWORK_EXISTS" = false ]; then
+				docker network create --scope=swarm --attachable -d overlay $NETWORK_NAME
+				echo "Network '$NETWORK_NAME' created successfully."
+		fi
+
+		# Deploy stack
+		docker stack config -c docker-compose.yml | docker stack deploy -c - outagealert
+
+		# Setup Doppler
+		curl -Ls https://cli.doppler.com/install.sh | sh
+		doppler configure set token $DOPPLER_TOKEN
 "
