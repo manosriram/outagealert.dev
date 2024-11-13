4ea4a3f33575fab1b16b4275c31ca35fa3408c12
Mano Sriram <mano.sriram0@gmail.com>
2024-11-13 18:39:42 +0530

    make network external

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,6 @@ ssh -v root@$OUTAGEALERT_IP "
 		export DOPPLER_TOKEN=$DOPPLER_TOKEN
 		echo $DOCKER_REGISTRY_PAT | docker login -u manosriram --password-stdin
 
-		NETWORK_NAME=outagenet
-
 		# Remove old image if exists
 		yes | (docker rmi manosriram/outagealert:app 2>/dev/null || true)
 
@@ -32,35 +30,6 @@ ssh -v root@$OUTAGEALERT_IP "
 		# Pull new image
 		docker pull manosriram/outagealert:app
 
-		# Stop existing stack and remove network
-		# docker stack rm outagealert
-
-		# Wait for stack to be fully removed
-		# while docker stack ls | grep -q 'outagealert'; do
-				# echo 'Waiting for stack to be removed...'
-				# sleep 5
-		# done
-
-		# sleep 5
-
-		# Check if network exists
-		# if ! docker network ls --format '{{.Name}}' | grep -q "^${NETWORK_NAME}$"; then
-				# echo "Creating overlay network: ${NETWORK_NAME}"
-				
-				# # Create network with error handling
-				# if ! docker network create \
-						# "${NETWORK_NAME}"; then
-						# echo "Error: Failed to create network ${NETWORK_NAME}"
-						# exit 1
-				# fi
-				
-				# echo "Network ${NETWORK_NAME} created successfully"
-		# else
-				# echo "Network ${NETWORK_NAME} already exists"
-		# fi
-
-		# Deploy stack
-		# docker stack config -c docker-compose.yml | docker stack deploy -c - outagealert
 		docker-compose down && docker-compose pull && docker-compose up -d --force-recreate
 
 		# Setup Doppler
