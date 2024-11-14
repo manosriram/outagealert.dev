fc1ca57d847f6641043c6e57743164d7a2fa9ef1
Mano Sriram <mano.sriram0@gmail.com>
2024-11-14 19:43:02 +0530

    Update backup and restore shell scripts

--- backup_psql.sh
+++ backup_psql.sh
@@ -1,2 +1,7 @@
-mkdir -p /mnt/volume_sgp1_02/psql_backups/
-docker exec -t postgres pg_dumpall -c -U postgres > /mnt/volume_sgp1_02/psql_backups/dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql;
+DATE=$(date +%Y-%m-%d_%H%M%S)
+BACKUP_PATH=/mnt/volume_sgp1_02/psql_backups
+DUMPFILE_NAME=dump_$DATE
+POSTGRES_CONTAINER_ID=$(docker ps -qf "name=outagealert-db-1")
+
+mkdir -p $BACKUP_PATH
+docker exec -it $POSTGRES_CONTAINER_ID pg_dumpall -c -U postgres > $BACKUP_PATH/$DUMPFILE_NAME.sql;
--- restore_psql.sh
+++ restore_psql.sh
@@ -1,1 +1,2 @@
-cat $1 | docker exec -i $2 psql -U $POSTGRES_USER;
+# ./restore_psql <file_path>
+cat $1 | docker exec -i docker ps -qf "name=outagealert-db-1" psql -U $POSTGRES_USER;
