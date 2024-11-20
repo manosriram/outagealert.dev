8b4bf2101862824a09aca329d00be74faf4426ce
Mano Sriram <mano.sriram0@gmail.com>
2024-11-20 17:23:40 +0530

    update known hosts path

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add host to known hosts
-yes | ssh-keyscan -R $OUTAGEALERT_IP >> ~/.ssh/known_hosts;
+ssh-keyscan -H $OUTAGEALERT_IP >> /home/circleci/.ssh/known_hosts;
 
 # Login to Docker registry
 echo $DOCKER_REGISTRY_PAT | docker login -u manosriram --password-stdin
