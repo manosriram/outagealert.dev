3d5e81e32709112e7c1f58e7dbb2a62a1026cccf
Mano Sriram <mano.sriram0@gmail.com>
2024-11-23 13:26:15 +0530

    fix: add Cache-Control header, separate middlewares

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gorilla/sessions"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
@@ -24,67 +20,12 @@ import (
 	"github.com/manosriram/outagealert.io/pkg/project"
 	t "github.com/manosriram/outagealert.io/pkg/template"
 	"github.com/manosriram/outagealert.io/pkg/types"
-	"github.com/manosriram/outagealert.io/sqlc/db"
 )
-
-func ToDashboardIfAuthenticated(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		s, err := session.Get("session", c)
-		if err != nil {
-			c.Error(err)
-		}
-
-		email := s.Values["email"]
-		if email == nil {
-			return next(c)
-		}
-		return c.Redirect(302, "/projects")
-	}
-}
-
-func IsAuthenticated(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		s, err := session.Get("session", c)
-		if err != nil {
-			c.Error(err)
-			return c.Redirect(302, "/signin")
-		}
-
-		email := s.Values["email"]
-		if email == nil {
-			return c.Redirect(302, "/signin")
-		}
-		return next(c)
-	}
-}
-
-func initDB() *db.Queries {
-	psqlUser := os.Getenv("POSTGRES_USER")
-	psqlPassword := os.Getenv("POSTGRES_PASSWORD")
-	psqlPort := os.Getenv("POSTGRES_PORT")
-	psqlDatabase := os.Getenv("POSTGRES_DATABASE")
-	psqlHost := os.Getenv("POSTGRES_HOST")
-
-	psqlString := fmt.Sprintf("user=%s password=%s port=%s database=%s sslmode=disable host=%s", psqlUser, psqlPassword, psqlPort, psqlDatabase, psqlHost)
-	l.Log.Info("psql string ", psqlString)
-	config, err := pgxpool.ParseConfig(psqlString)
-	if err != nil {
-		l.Log.Infof("Unable to parse connection string: %v", err)
-	}
-	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeCacheDescribe
-
-	pool, err := pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
-	}
-	dbconn := db.New(pool)
-	return dbconn
-}
 
 func main() {
 	e := echo.New()
 	e.Renderer = t.NewTemplate()
-	l.Init() // Initialize logger
+	l.Init()
 
 	rateLimiterConfig := middleware.RateLimiterConfig{
 		Skipper: middleware.DefaultSkipper,
@@ -113,9 +54,10 @@ func main() {
 
 	e.Static("/static", "static")
 
-	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))))
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+	e.Use(TagDefaultResponseHeaders)
 
 	dbconn := initDB()
 	env := types.NewEnv(dbconn)
